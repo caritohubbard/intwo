71fb5d030f7e391d4dbf95f6ff7de122030d31cb
caritohubbard <carohubbard35@gmail.com>
2025-06-25 20:59:44 -0700

    modified cnt = 0 to cnt = 1 so it doesnt always stick to 0. fized multiple readesrs reading simultaneously. writers now have exclusive access.

--- esclect.c
+++ esclect.c
@@ -1,56 +1,84 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 sem_t wrt;
 pthread_mutex_t mutex;
 int readrnum = 0;
-int cnt = 0;
+int cnt = 1;
 
-void *writer(void *wno){
+void *writer(void *wno) {
+    int id = *((int *)wno);
     sem_wait(&wrt);
-    cnt = cnt*2;
-    printf("Writer %d modified cnt to %d\n", (*((int *)wno)),cnt);
+
+    printf("Writer %d is writing...\n", id);
+    cnt = cnt * 2;
+    printf("Writer %d modified cnt to %d\n", id, cnt);
+    sleep(1);
+
     sem_post(&wrt);
+    return NULL;
 }
-void *reader(void *rno){
+void *reader(void *rno) {
+    int id = *((int *)rno);
+
     pthread_mutex_lock(&mutex);
     readrnum++;
-    if(readrnum == 1)
-        sem_wait(&wrt);
+    if (readrnum == 1) {
+        sem_wait(&wrt);  
+    }
     pthread_mutex_unlock(&mutex);
 
-    printf("Reader %d read cnt as %d\n", (*((int *)rno)),cnt);
+    printf("Reader %d is reading... cnt = %d\n", id, cnt);
+    sleep(1);
 
     pthread_mutex_lock(&mutex);
     readrnum--;
-    if(readrnum == 0)
-        sem_post(&wrt);
+    if (readrnum == 0) {
+        sem_post(&wrt); 
+    }
     pthread_mutex_unlock(&mutex);
+
+    return NULL;
 }
 
-int main(){
+int main() {
     pthread_t read[10], write[5];
     sem_init(&wrt, 0, 1);
+    pthread_mutex_init(&mutex, NULL);
 
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int a[10];
+    for (int i = 0; i < 10; i++) a[i] = i + 1;
+
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&read[i], NULL, reader, &a[i]);
+    }
+
+    sleep(1);
+
+    for (int i = 0; i < 2; i++) {
+        pthread_create(&write[i], NULL, writer, &a[i]);
+    }
+
+    for (int i = 5; i < 10; i++) {
+        pthread_create(&read[i], NULL, reader, &a[i]);
+    }
+
+    for (int i = 2; i < 5; i++) {
+        pthread_create(&write[i], NULL, writer, &a[i]);
+    }
 
     for (int i = 0; i < 10; i++) {
-        pthread_create(&read[i], NULL, (void *)reader, (void *)&a[i]);
-    }
-
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&write[i], NULL, (void *)writer, (void *)&a[i]);
-    }
-
-    for(int i = 0; i < 10; i++) {
         pthread_join(read[i], NULL);
     }
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) {
         pthread_join(write[i], NULL);
     }
+
     pthread_mutex_destroy(&mutex);
     sem_destroy(&wrt);
+
     return 0;
 }
